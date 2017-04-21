b36f347642e6eb4e65172344c324dbff753a847f
Pedro Brandao <pcrbrandao@gmail.com>
2017-04-21 11:49:32 -0300

    acrescentando VeloxLogin depois de atualizar gitignore

---
+++ VeloxLogin/Assets/Scripts/Licenca.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class Licenca : MonoBehaviour {
+
+	int Codigo { get; set; }
+	DateTime Date { get; set; }
+	Usuario Usuario { get; set; }
+}
